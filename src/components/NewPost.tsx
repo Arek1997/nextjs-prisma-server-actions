4828aead350844bd71cb8117578fd269276a371b
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-11-14 19:50:22 +0100

    Add logic and state to modal

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -11,26 +11,58 @@ import {
   Input,
   Textarea,
 } from "@nextui-org/react";
+import { useState } from "react";
 
 const NewPosts = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [response, setResponse] = useState("");
+
+  const addNewPostHandler = async (callBack: () => void) => {
+    if (!title.trim() || !content.trim()) {
+      return setResponse(`Title and content are required.`);
+    }
+
+    try {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      callBack();
+    } catch (error) {}
+  };
 
   return (
     <>
       <Button className="mb-5 justify-self-center" onPress={onOpen}>
-        Open Modal
+        New posts
       </Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader>New Post</ModalHeader>
+              <ModalHeader className="grid gap-2">
+                New Post
+                {response && (
+                  <p className="text-sm text-danger-400">{response}</p>
+                )}
+              </ModalHeader>
               <ModalBody>
                 <Input
                   isRequired
                   type="text"
                   label="TITLE"
                   variant="underlined"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  isInvalid={title.trim().length !== 2}
                 />
                 <Input
                   type="file"
@@ -38,13 +70,23 @@ const NewPosts = () => {
                   placeholder=" "
                   variant="underlined"
                 />
-                <Textarea isRequired variant="underlined" label="Content" />
+                <Textarea
+                  isRequired
+                  variant="underlined"
+                  label="Content"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  isInvalid={content.trim().length !== 5}
+                />
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Cancel
                 </Button>
-                <Button color="default" onPress={onClose}>
+                <Button
+                  color="default"
+                  onPress={() => addNewPostHandler(onClose)}
+                >
                   Accept
                 </Button>
               </ModalFooter>
