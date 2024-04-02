c07fca13046caaa4f7ca1652319183295b17b2e9
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2024-04-02 20:25:59 +0200

    Create login page and login action

---
+++ src/app/(register)/login/actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import prisma from "@/libs/prisma";
+import { z } from "zod";
+import bcript from "bcrypt";
+import { permanentRedirect } from "next/navigation";
+import Session from "@/services/session";
+import { emailSchema } from "@/schema/email";
+import { passwordSchema } from "@/schema/password";
+
+const loginSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+});
+
+export const userLogin = async (_: unknown, formData: FormData) => {
+  const result = loginSchema.safeParse(Object.fromEntries(formData.entries()));
+
+  if (!result.success) {
+    const { message, path } = result.error.issues[0];
+
+    return {
+      error: message,
+      invalidElement: path[0],
+    };
+  }
+
+  const { email, password } = result.data;
+
+  const user = await prisma.users.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  const passwordMatch = await bcript.compare(password, user?.password || "");
+
+  if (!user || !passwordMatch) {
+    return {
+      error: `Invalid credentials`,
+      invalidElement: "response",
+    };
+  }
+
+  Session().create(user);
+  permanentRedirect("/");
+};
---
+++ src/app/(register)/login/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Button, Input } from "@nextui-org/react";
+import Link from "next/link";
+import { useFormState, useFormStatus } from "react-dom";
+import { userLogin } from "./actions";
+
+const Login = () => {
+  const [state, formAction] = useFormState(userLogin, {
+    error: "",
+    invalidElement: "",
+  });
+
+  return (
+    <div className="mx-auto mt-20 max-w-[500px] rounded-xl p-5 md:p-14">
+      <h1 className="mb-8">Login</h1>
+
+      <form action={formAction} className="space-y-4">
+        {state.invalidElement === "response" && (
+          <p className="text-sm text-red-400">{state?.error}</p>
+        )}
+        <div>
+          <Input
+            label="Email"
+            name="email"
+            isInvalid={state?.invalidElement === "email"}
+            errorMessage={state?.invalidElement === "email" && state?.error}
+            isRequired
+          />
+        </div>
+        <div>
+          <Input
+            label="Password"
+            type="password"
+            name="password"
+            isInvalid={state?.invalidElement === "password"}
+            errorMessage={state?.invalidElement === "password" && state?.error}
+            isRequired
+          />
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <p className="text-sm">
+            Have not an account?{" "}
+            <Link href="/signup" className="underline">
+              Signup
+            </Link>
+          </p>
+
+          <SubmitButton />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      color="secondary"
+      type="submit"
+      className="ml-auto flex"
+      isLoading={pending}
+      isDisabled={pending}
+    >
+      Login
+    </Button>
+  );
+};
+
+export default Login;
