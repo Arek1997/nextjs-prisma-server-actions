3bfde21caf3c77b1a9b3ddb824582600fa5a5b61
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2024-04-21 20:18:51 +0200

    Modify navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,8 +6,8 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
   Button,
+  Link,
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
@@ -15,6 +15,7 @@ import {
 import Logo from "@/components/Logo";
 import { usePathname } from "next/navigation";
 import ThemeSwitch from "./ThemeSwitch";
+import { logOutHandler } from "@/app/actions/logout";
 
 const menuItems = [
   {
@@ -22,14 +23,16 @@ const menuItems = [
     href: "/posts",
   },
   {
-    name: "Log Out",
-    href: null,
+    name: "Profil",
+    href: "/profil",
   },
 ] as const;
 
-const Navigation = () => {
+type Props = {
+  hasSession: string | undefined;
+};
+const Navigation = ({ hasSession }: Props) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const pathname = usePathname();
 
   return (
@@ -40,11 +43,13 @@ const Navigation = () => {
       }}
     >
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-expanded={isMenuOpen}
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
+        {hasSession && (
+          <NavbarMenuToggle
+            aria-expanded={isMenuOpen}
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+        )}
         <NavbarBrand>
           <Link href="/" color="foreground">
             <Logo />
@@ -53,53 +58,59 @@ const Navigation = () => {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
-        {menuItems.map(({ name, href }, index) => {
-          if (name === "Log Out") return;
-
-          return (
-            <NavbarItem isActive={href === pathname} key={`${name}-${index}`}>
-              <Link
-                href={href}
-                aria-current={href === pathname ? "page" : "false"}
-                color={href === pathname ? "primary" : "foreground"}
-              >
-                {name}
-              </Link>
-            </NavbarItem>
-          );
-        })}
-      </NavbarContent>
+      {hasSession && (
+        <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+          {menuItems.map(({ name, href }, index) => {
+            return (
+              <NavbarItem isActive={href === pathname} key={`${name}-${index}`}>
+                <Link
+                  href={href}
+                  aria-current={href === pathname ? "page" : "false"}
+                  color={href === pathname ? "primary" : "foreground"}
+                >
+                  {name}
+                </Link>
+              </NavbarItem>
+            );
+          })}
+        </NavbarContent>
+      )}
       <NavbarContent justify="end">
         <ThemeSwitch />
         <NavbarItem>
-          <Button
-            as={Link}
-            color="primary"
-            href={isLoggedIn ? "/logout" : "/signup"}
-            variant="flat"
-          >
-            {isLoggedIn ? "Log Out" : "Sign Up"}
-          </Button>
+          {hasSession ? (
+            <Button
+              color="primary"
+              variant="flat"
+              onPress={() => logOutHandler()}
+            >
+              Log Out
+            </Button>
+          ) : (
+            <Button as={Link} href="/signup" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          )}
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu>
-        {menuItems.map(({ name, href }, index) => {
-          if (name === "Log Out") return;
-          return (
-            <NavbarMenuItem key={`${name}-${index}`}>
-              <Link
-                color={href === pathname ? "primary" : "foreground"}
-                aria-current={href === pathname ? "page" : "false"}
-                className="w-full"
-                href={href}
-              >
-                {name}
-              </Link>
-            </NavbarMenuItem>
-          );
-        })}
-      </NavbarMenu>
+      {hasSession && (
+        <NavbarMenu>
+          {menuItems.map(({ name, href }, index) => {
+            return (
+              <NavbarMenuItem key={`${name}-${index}`}>
+                <Link
+                  color={href === pathname ? "primary" : "foreground"}
+                  aria-current={href === pathname ? "page" : "false"}
+                  className="w-full"
+                  href={href}
+                >
+                  {name}
+                </Link>
+              </NavbarMenuItem>
+            );
+          })}
+        </NavbarMenu>
+      )}
     </Navbar>
   );
 };
