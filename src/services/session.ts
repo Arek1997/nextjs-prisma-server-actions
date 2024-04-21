fea64f16adfb4e210805c258ad0f121f4b32a41c
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2024-04-21 20:18:23 +0200

    Add services

--- src/services/session.ts
+++ src/services/session.ts
@@ -1,22 +1,21 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
 import { cookies } from "next/headers";
+import Jwt from "./jwt";
 
 export default function Session() {
   const SESSION_KEY = "session";
-  const secretKey: string = process.env.JWT_SECRET!;
   const expiresInSecond = 3600 as const; // 1h
 
   function create(data: any) {
-    const session = jwt.sign(data, secretKey, {
+    const token = Jwt().createToken(data, {
       expiresIn: expiresInSecond,
     });
 
     const expires = new Date(Date.now() + expiresInSecond * 1000); // 1h from now
-    return cookies().set(SESSION_KEY, session, { expires, httpOnly: true });
+    return cookies().set(SESSION_KEY, token, { expires, httpOnly: true });
   }
 
   function get(key: string = SESSION_KEY) {
-    return cookies().get(key)?.value;
+    return cookies().get(key)?.value || "";
   }
 
   function destroy(key: string = SESSION_KEY) {
