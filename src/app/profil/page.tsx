729592fdfc7ec9708a4a2b82b0fa8216e7011538
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2024-05-05 16:43:36 +0200

    Add utils, update profile page

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { getUserById } from "../actions/getUser";
 import ProfilActions from "./components/ProfilActions";
+import { formatDate } from "@/utils/functions";
 
 const ProfilPage = async () => {
   const currentUser = await getUserById();
@@ -10,24 +11,24 @@ const ProfilPage = async () => {
   }
 
   return (
-    <section className="mt-10 md:mx-auto md:max-w-[400px]">
+    <section className="mt-10 md:mx-auto md:max-w-[500px]">
       <article className="space-y-4 rounded-xl border border-black p-4 text-left dark:border-white">
         <h1 className="text-center font-bold">Profil details</h1>
         <p className="flex items-center justify-between gap-4">
-          Profile name: <span>{currentUser.name}</span>
+          Name: <span>{currentUser.name}</span>
         </p>
         <p className="flex items-center justify-between gap-4">
-          Profile email:
+          Email:
           <span>{currentUser.email}</span>
         </p>
         <p className="flex items-center justify-between gap-4">
-          Profile created at:
-          <span> {new Date(currentUser.createdAt).toLocaleDateString()}</span>
+          Created at:
+          <span> {formatDate(currentUser.createdAt)}</span>
         </p>
         {currentUser.modifiedAt && (
           <p className="flex items-center justify-between gap-4">
             Last modification:
-            <span>{new Date(currentUser.modifiedAt).toLocaleDateString()}</span>
+            <span>{formatDate(currentUser.modifiedAt)}</span>
           </p>
         )}
       </article>
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -4,3 +4,7 @@ export const wait = async (seconds: number) =>
   new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
 export const generateRandomToken = () => crypto.randomBytes(32).toString("hex");
+
+export const formatDate = (date: Date | string) => {
+  return new Date(date).toUTCString().split("GMT")[0];
+};
