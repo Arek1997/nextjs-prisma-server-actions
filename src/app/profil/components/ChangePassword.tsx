8df948a8382227b4a95c0d4a47cdbfcf14b0d935
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2024-05-13 16:10:44 +0200

    Update UI

--- src/app/profil/components/ChangePassword.tsx
+++ src/app/profil/components/ChangePassword.tsx
@@ -1,70 +1,45 @@
-import { useState } from "react";
 import { useFormState } from "react-dom";
-import { Button } from "@nextui-org/react";
-import { motion, AnimatePresence } from "framer-motion";
 import InputWithEye from "@/components/InputWithEye";
 import SubmitButton from "@/components/SubmitButton";
 import { changePassword } from "../actions";
 
 const ChangePassword = () => {
-  const [showInputs, setShowInputs] = useState(false);
   const [state, formAction] = useFormState(changePassword, {
     error: "",
     invalidElement: "",
   });
 
   return (
-    <div className="mx-auto mt-4 space-y-4 text-left">
-      <Button
-        variant="ghost"
-        disableAnimation
-        onPress={() => setShowInputs((prevState) => !prevState)}
-      >
-        Change password
-      </Button>
-
+    <div className="mx-auto mt-8 space-y-4 text-left">
       <p className="text-xs font-bold underline">
         After success password change, you will be logged out.
       </p>
 
-      <AnimatePresence>
-        {showInputs && (
-          <motion.form
-            action={formAction}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <InputWithEye
-              name="old-password"
-              label="Current password"
-              variant="underlined"
-              isInvalid={state?.invalidElement === "old-password"}
-              errorMessage={
-                state?.invalidElement === "old-password" && state?.error
-              }
-              autoFocus
-              isRequired
-            />
-            <InputWithEye
-              name="new-password"
-              label="New password"
-              variant="underlined"
-              isInvalid={state?.invalidElement === "new-password"}
-              errorMessage={
-                state?.invalidElement === "new-password" && state?.error
-              }
-              isRequired
-            />
+      <form action={formAction}>
+        <InputWithEye
+          name="old-password"
+          label="Current password"
+          variant="underlined"
+          isInvalid={state?.invalidElement === "old-password"}
+          errorMessage={
+            state?.invalidElement === "old-password" && state?.error
+          }
+          autoFocus
+          isRequired
+        />
+        <InputWithEye
+          name="new-password"
+          label="New password"
+          variant="underlined"
+          isInvalid={state?.invalidElement === "new-password"}
+          errorMessage={
+            state?.invalidElement === "new-password" && state?.error
+          }
+          isRequired
+        />
 
-            <SubmitButton
-              className="mt-4"
-              text="Change"
-              pendingText="Changing"
-            />
-          </motion.form>
-        )}
-      </AnimatePresence>
+        <SubmitButton className="mt-4" text="Change" pendingText="Changing" />
+      </form>
     </div>
   );
 };
