a21a8f1ce027f43f9e00d8f0c33b114ec848761e
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2024-06-29 17:25:47 +0200

    Created schema file

--- src/app/posts/actions.ts
+++ src/app/posts/actions.ts
@@ -2,34 +2,8 @@
 
 import prisma from "@/libs/prisma";
 import { revalidatePath } from "next/cache";
-import { z } from "zod";
+import { post, comment } from "./schema";
 import { auth } from "@/auth";
-
-const post = z.object({
-  title: z
-    .string({
-      required_error: "Post title required",
-    })
-    .min(5, {
-      message: "Title has to be at least 5 characters",
-    }),
-  image: z.string().optional(),
-  message: z
-    .string({
-      required_error: "Post message required",
-    })
-    .min(10, {
-      message: "Title has to be at least 10 characters",
-    }),
-});
-
-const comment = z
-  .string({
-    required_error: "Comment can not be empty",
-  })
-  .min(3, {
-    message: "Comment has to be at least 3 characters",
-  });
 
 export const createPost = async (_: unknown, formData: FormData) => {
   const result = post.safeParse(Object.fromEntries(formData.entries()));
